51001ec5900ea04bdc7818ca74c4f14664f291f9
Ryan Hartadi <ryanhartadi999@gmail.com>
2023-05-18 17:58:02 +0700

    Readable Document

--- route/route.go
+++ route/route.go
@@ -3,8 +3,10 @@ package route
 import (
 	"Go-Echo/controller"
 	"Go-Echo/middleware"
+	"fmt"
 	"github.com/labstack/echo/v4"
 	mid "github.com/labstack/echo/v4/middleware"
+	"net/http"
 )
 
 func New() *echo.Echo {
@@ -16,7 +18,16 @@ func New() *echo.Echo {
 
 	e.GET("/product", controller.GetProductController)
 	e.POST("/product", controller.StoreProductController)
+	e.GET("/page2/:name", func(ctx echo.Context) error {
+		name := ctx.Param("name")
+		data := fmt.Sprintf("Hello %s", name)
 
+		return ctx.String(http.StatusOK, data)
+	})
+	e.GET("/uploads/:imageName", func(c echo.Context) error {
+		filename := c.Param("imageName")
+		return c.File("uploads/" + filename)
+	})
 	eAuthBasic := e.Group("/auth")
 	eAuthBasic.Use(mid.BasicAuth(middleware.BasicAuthLogin))
 	eAuthBasic.GET("/user", controller.GetUserController)
